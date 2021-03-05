ce53f83e183977ba16e21cc5eaece57631df7227
leonlol-dev <59918677+leonlol-dev@users.noreply.github.com>
2021-03-05 11:07:55 +0000

    Test Enemy
    not finished
    
    need a player

---
+++ GSP-Assignment/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    Transform player;
+    NavMeshAgent enemy; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
