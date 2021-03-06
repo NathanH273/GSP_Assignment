05185656c4e00a90ded2ccf66e3b656dfe3d74df
leonlol-dev <59918677+leonlol-dev@users.noreply.github.com>
2021-03-06 21:55:06 +0000

    enemycontroller.cs

--- GSP-Assignment/Assets/Scripts/Controllers/EnemyController.cs
+++ GSP-Assignment/Assets/Scripts/Controllers/EnemyController.cs
@@ -13,7 +13,7 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = PlayerManager.instance.player.transform; 
+        //player = PlayerManager.instance.player.transform; 
         enemy = GetComponent<NavMeshAgent>();
     }
 
