1da2df7821423eeaff280e7c2cb1e551e9859b8d
LewisRobertson15 <74059857+LewisRobertson15@users.noreply.github.com>
2021-03-16 14:10:37 +0000

    sword

--- GSP-Assignment/Assets/Scripts/Sword.cs
+++ GSP-Assignment/Assets/Scripts/Sword.cs
@@ -1,20 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class SwordStuff : MonoBehaviour
+public class Sword : MonoBehaviour
 {
-    [SerializeField] public Transform Sword;
-    [SerializeField] public GameObject Enemy;
-    [SerializeField] public GameObject Pivot;
+    public Transform Sword;
+    public GameObject Enemy;
+    public GameObject Pivot;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Sword.transform.Rotate(0, -110, 0, Space.Pivot);
         }
-        Sword.transform.Rotate(0, 0, 0, Space.Pivot);
+        Sword.transform.Rotate(0, 110, 0, Space.Pivot);
     }
 
 
