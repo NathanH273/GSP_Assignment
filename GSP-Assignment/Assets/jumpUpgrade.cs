dd1f19b95a17e49a351167aaf468fefe78c84599
NathanH <s5224787@bournemouth.ac.uk>
2021-03-23 17:50:48 +0000

    Jump
    double jump + jump upgrade

---
+++ GSP-Assignment/Assets/jumpUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpUpgrade : MonoBehaviour
+{
+    public ThirdPersonCharacterController player;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        player.jumpUpgrade = true;
+        Destroy(gameObject);
+    }
+}
