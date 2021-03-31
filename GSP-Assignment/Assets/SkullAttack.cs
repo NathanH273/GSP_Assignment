568e564bc346063da5167e7c864b78e427c807df
leonlol-dev <59918677+leonlol-dev@users.noreply.github.com>
2021-03-31 22:58:02 +0100

    skull enemy done
    mostly everything working, let me know if u witness any bugs

---
+++ GSP-Assignment/Assets/SkullAttack.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullAttack : MonoBehaviour
+{
+    //Player Attributes
+    public Transform playerModel;
+    public GameObject explosionParticles;
+
+    //Enemy Skull Attributes
+    private int currentHealth;
+    public int maxHealth = 50;
+
+    //Enemy Health Bar and other misc
+    public HealthBarScript healthbar;
+    public AudioClip explosionSound;
+    public EnemyController enemyScript;
+    
+
+    void Start()
+    {
+        //explosionSound = GetComponent<AudioSource>();
+        //healthbar = GetComponent<HealthBarScript>();
+
+        //Set the enemy health to the max health.
+        currentHealth = maxHealth;
+
+        //Set the UI health bar to the enemy's max health.
+        healthbar.SetMaxHealth(maxHealth);
+
+        EnemyController enemyScript = GetComponent<EnemyController>();
+    }
+
+    void Update()
+    {
+        if(playerModel != null && enemyScript.lookRadius <= enemyScript.distance )
+        {
+            transform.LookAt(playerModel);
+        }
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void takeDamage(int damage)
+    {
+        //When player takes damage remove that amount damage to player's current HP
+        currentHealth -= damage;
+
+        //Update UI health bar
+        healthbar.SetHealth(currentHealth);
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+
+        if (collision.transform == playerModel)
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+            Instantiate(explosionParticles, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Debug.Log("skull suicide");
+
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            takeDamage(5);
+        }
+
+        if (collision.gameObject.tag == "bullet")
+        {
+            takeDamage(5);
+        }
+
+
+
+    }
+}
+
