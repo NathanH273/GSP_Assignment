ac7f75f77aa390e2f8f2bdff071a6559d22fbc55
leonlol-dev <59918677+leonlol-dev@users.noreply.github.com>
2021-04-04 21:06:18 +0100

    navmeshagent assets and ranged enemy AI
    ranged enemy that patrols the map and once it spots the player will start to shoot at him (only patrols doesn't shoot the player yet)

--- GSP-Assignment/Assets/RangedEnemy.cs
+++ GSP-Assignment/Assets/RangedEnemy.cs
@@ -14,6 +14,7 @@ public class RangedEnemy : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask ground, whatIsPlayer;
+    public GameObject projectile;
 
     //Patrolling 
     public Vector3 walkPoint;
@@ -32,7 +33,7 @@ public class RangedEnemy : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("player").transform;
+        player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -40,6 +41,93 @@ public class RangedEnemy : MonoBehaviour
     {
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange)
+        {
+            Patrolling();
+        }
+
+        if(playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+
+        if(playerInAttackRange && playerInSightRange)
+        {
+            AttackPlayer();
+        }
     }
 
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, ground))
+        {
+            walkPointSet = true;
+        }
+
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+
+
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), attackSpeed);
+
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
 }
