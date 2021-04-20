b91a36608bd4952952b1e1422c05c4adda67b3ba
leonlol-dev <59918677+leonlol-dev@users.noreply.github.com>
2021-04-20 20:38:27 +0100

    GUN updated + GUN HUD
    added:

---
+++ GSP-Assignment/Assets/AmmoHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class AmmoHUD : MonoBehaviour
+{
+    //Player
+    public GunV2 gun;
+
+    //Text
+    public TextMeshProUGUI text;
+
+
+    public void SetAmmo(int ammo, int maxAmmo)
+    {
+        text.text = ammo + "/" + maxAmmo;
+    }
+
+
+}
